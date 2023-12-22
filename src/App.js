8a88cd73d84f801012b39de70f12c27e02292dcd
Dustin <dustin@leapit.us>
2023-12-22 13:04:30 -0600

    Added OpenAI Chat

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import HeroSection from './components/HeroSection';
 import ActivitiesSection from './components/ActivitiesSection';
 import Footer from './components/Footer';
-import ChatBox from './components/ChatBox'; // Import the ChatBox component
+import ChatBox from './components/ChatBox';
 import './App.css';
 
 const App = () => {
@@ -11,7 +11,7 @@ const App = () => {
     <div className="app">
       <HeroSection />
       <ActivitiesSection />
-      <ChatBox /> {/* Add the ChatBox component here */}
+      <ChatBox />
       <Footer />
     </div>
   );
--- src/components/ChatBox.js
+++ src/components/ChatBox.js
@@ -1,29 +1,59 @@
 import React, { useState, useRef, useEffect } from 'react';
 import './ChatBox.css';
+import axios from 'axios';
 
-const MAX_CONVERSATION_PAIRS = 2; // Set your desired maximum number of conversation pairs
+const MAX_CONVERSATION_PAIRS = 1;
 
 const ChatBox = () => {
   const [conversation, setConversation] = useState([]);
   const [inputValue, setInputValue] = useState('');
+  const [loading, setLoading] = useState(false);
   const chatBoxRef = useRef(null);
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
   };
 
-  const handleSendMessage = () => {
+  const handleSendMessage = async () => {
     if (inputValue.trim() !== '') {
-      const newConversation = [...conversation, { user: inputValue, bot: 'Bot response' }];
-      setConversation(newConversation.slice(-MAX_CONVERSATION_PAIRS));
-      setInputValue('');
-      window.scrollTo(0, document.body.scrollHeight);
-    }
-  };
+      try {
+        setLoading(true);
 
-  const handleRefresh = () => {
-    setConversation([]);
-    setInputValue('');
+        const response = await axios.post(
+          'https://indy-specialist.openai.azure.com/openai/deployments/Indy-Specialist/chat/completions?api-version=2023-07-01-preview',
+          {
+            messages: [
+              { role: 'system', content: 'Your name is HoosierHelper and you are an AI Assistant. You are an expert in all things related to Indianapolis and general knowledge.' },
+              { role: 'user', content: inputValue },
+            ],
+            temperature: 0.7,
+            max_tokens: 50,
+            top_p: 0.95,
+            frequency_penalty: 0,
+            presence_penalty: 0,
+            stop: null,
+          },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+              'api-key': '5b196d222c9a4a3c9655a25c6680b2ec',
+            },
+          }
+        );
+
+        const botResponse = response.data.choices[0].message.content;
+
+        const newConversation = [...conversation, { user: inputValue, bot: botResponse }];
+        setConversation(newConversation.slice(-MAX_CONVERSATION_PAIRS));
+        setInputValue('');
+        setLoading(false);
+        window.scrollTo(0, document.body.scrollHeight);
+      } catch (error) {
+        console.error('Error making API request:', error);
+        setLoading(false);
+        // Handle error as needed
+      }
+    }
   };
 
   const handleKeyDown = (e) => {
@@ -37,30 +67,46 @@ const ChatBox = () => {
     chatBoxRef.current.scrollTop = chatBoxRef.current.scrollHeight;
   }, [conversation]);
 
+  // Conditional rendering of placeholder bot message
+  const renderPlaceholderBotMessage = () => {
+    if (conversation.length === 0) {
+      return (
+        <div className="message bot-message">
+          Hello! How can I help you?
+        </div>
+      );
+    }
+    return null;
+  };
+
   return (
-    <div className="chat-box-container">
+    <div className="chat-container">
       <div className="chat-header">
-        <h2>Still not convinced?</h2>
-        <p>Ask our resident Hoosier specialist anything you want to know about Indy.</p>
+        <h2>Still not convinced?!</h2>
+        <p>Ask our Hoosier expert anything else you'd like to know.</p>
       </div>
       <div className="chat-box" ref={chatBoxRef}>
+        {renderPlaceholderBotMessage()}
         {conversation.map((pair, index) => (
           <React.Fragment key={index}>
-            <div className="message user-message">{pair.user}</div>
-            <div className="message bot-message">{pair.bot}</div>
+            <div className={`message user-message ${loading && index === conversation.length - 1 ? 'loading' : ''}`}>
+              {loading && index === conversation.length - 1 ? '...' : pair.user}
+            </div>
+            <div className={`message bot-message ${loading && index === conversation.length - 1 ? 'loading' : ''}`}>
+              {loading && index === conversation.length - 1 ? '...' : pair.bot}
+            </div>
           </React.Fragment>
         ))}
-      </div>
-      <div className="input-container">
-        <button onClick={handleRefresh}>Refresh</button>
-        <input
-          type="text"
-          value={inputValue}
-          onChange={handleInputChange}
-          onKeyDown={handleKeyDown}
-          placeholder="Type your message..."
-        />
-        <button onClick={handleSendMessage}>Send</button>
+        <div className="input-container">
+          <input
+            type="text"
+            value={inputValue}
+            onChange={handleInputChange}
+            onKeyDown={handleKeyDown}
+            placeholder="Please enter your question..."
+          />
+          <button className="send-button" onClick={handleSendMessage}>Send</button>
+        </div>
       </div>
     </div>
   );
